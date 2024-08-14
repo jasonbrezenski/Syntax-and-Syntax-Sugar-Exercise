c62182d57eccb171f3097392e673a1b966d7fc87
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-14 13:39:09 -0500

    added ternary operator

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SyntaxSugarExercise();
+        }
+
+        public static void SyntaxSugarExercise()
+        {
+            //int answer = 4;  //explicit typing
+            //string response;  //explicit typing
+            
+            //if (answer < 9)
+            //{
+                //response = $"{answer} is less than nine.";
+            //}
+            //else
+            //{
+                //response = $"{answer} is greater than or equal to nine.";
+            //}
+
+            var answer = 4;  //inferred typing
+            var answer2 = 9;  //inferred typing
+
+            var response = answer < answer2
+                ? $"{answer} is less than {answer2}."
+                : $"{answer} is greater than or equal to {answer2}.";
+            
+            Console.WriteLine(response);
         }
     }
 }
